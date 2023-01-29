f1303818e9c778a49dd8bcef70427fe0dbee3504
Tetiana Nivchyk <tata.02od@gmail.com>
2023-01-29 10:02:58 +0200

    added the 2nd task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//REFERENCES
+const galleryContainerRef = document.querySelector('.gallery');
+
+//Creating markup
+galleryContainerRef.insertAdjacentHTML('afterbegin', creatingMarkup(galleryItems));
+
+function creatingMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `<li><a href="${original}" class="gallery__link">
+  <img class="gallery__image" src="${preview}" alt="${description}"/></a></li>`;
+    })
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoomFactor: 0.1,
+});
+
